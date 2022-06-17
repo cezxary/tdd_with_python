ea5c5f300492241ec5bf3b200cb0a5f480102b69
cezary <czarek_94@interia.eu>
2022-06-17 18:43:49 +0200

    FTs updated - extracted test for multiple lists

--- lists/models.py
+++ lists/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 
 # Create your models here.
+# TODO: Adjust model so that items are associated with different lists
 class Item(models.Model):
     text = models.TextField(default='')
--- lists/views.py
+++ lists/views.py
@@ -3,6 +3,9 @@ from lists.models import Item
 
 
 # TODO: support more than one list
+# TODO: add unique URL for each list
+# TODO: add a URL for creating a new list via POST
+# TODO: add URLs for adding a new item to an existing list via POST
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
