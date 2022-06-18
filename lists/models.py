065c1e879a6b8d8de0be45b52a08508e5412bce8
cezary <czarek_94@interia.eu>
2022-06-18 17:54:56 +0200

    List object added to db

--- lists/models.py
+++ lists/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 
 # Create your models here.
+class List(models.Model):
+    pass
+
+
 # TODO: Adjust model so that items are associated with different lists
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=0, on_delete=models.CASCADE)
