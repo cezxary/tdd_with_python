76ecc0a3e61bda2156fe5b6f4aa21b611928f2b7
cezary <czarek_94@interia.eu>
2022-06-22 22:07:52 +0200

    List URLs added as an app instead of direct linking in superlists/urls.py

--- lists/urls.py
+++ lists/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import include, path
-from lists import views as list_views
-from lists import urls as list_urls
+from django.urls import path
+from lists import views
 
 # TODO: similar-looking lists - find a way to refactor duplicates
 urlpatterns = [
-    path('', list_views.home_page, name='home'),
-    path('lists/', include(list_urls))
+    path('', views.home_page),
+    path('new', views.new_list, name='new_list'),
+    path('<int:list_id>/', views.view_list, name='view_list'),
+    path('<int:list_id>/add_item/', views.add_item, name='add_item')
 ]
