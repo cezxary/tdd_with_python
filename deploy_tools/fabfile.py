6e669261e509b333d4aeb0b992c82747fd283125
cezxary <74569453+cezxary@users.noreply.github.com>
2022-07-13 12:45:40 +0200

    fabfile fixed - automated updates to small.pl fully functional

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -32,16 +32,17 @@ def _deploy(c: Connection, site_type):
 
     _create_virtualenv(c)
     _create_devil_python_site(c, site_type, target_site)
-    _update_bash_profile(c, target_site)
+    # _update_bash_profile(c, target_site)
     site_folder = f'~/domains/{target_site}/public_python'
     with c.cd(site_folder):
         _get_latest_source(c)
         _update_virtualenv(c)
         _create_or_update_dotenv(c, target_site)
+        # _create_or_update_env_var_dotpy(c, target_site)
         with c.prefix('source .env'):
             _update_static_files(c)
             _update_database(c)
-            _move_devil_passenger_config(c)
+            _copy_devil_passenger_config(c)
             _update_passenger_config(c)
             _restart_devil_app(c, target_site)
     c.close()
@@ -72,12 +73,31 @@ def _get_latest_source(c):
         print('INFO: git repo fetched')
     git_log_return = subprocess.run(['git', 'log', '-n', '1', '--format=%H'], capture_output=True).stdout
     current_commit = git_log_return.decode('utf-8').strip()
-    c.run('pwd')
     c.run(f'git reset --hard {current_commit}')
 
 
 def _update_virtualenv(c):
     c.run('~/venvs/tdd-django/bin/pip install -r requirements.txt')
+
+
+def _create_or_update_env_var_dotpy(c, site_name):
+    create_or_update_expr = 'grep -qxF "{0}" env_var.py || echo "{0}" >> env_var.py'
+
+    create_or_update_header = create_or_update_expr.format('import os')
+    c.run(create_or_update_header)
+
+    create_or_update_debug = create_or_update_expr.format('os.environ[\'DJANGO_DEBUG_FALSE\'] = \'y\'')
+    c.run(create_or_update_debug)
+
+    create_or_update_sitename = create_or_update_expr.format(f'os.environ[\'SITENAME\'] = \'{site_name}\'')
+    c.run(create_or_update_sitename)
+
+    current_contents = c.run('cat env_var.py').stdout
+    if 'DJANGO_SECRET_KEY' not in current_contents:
+        new_secret = ''.join(random.SystemRandom().choices(
+            'abcdefghijklmnopqrstuvwxyz0123456789', k=50
+        ))
+        c.run(f'echo "os.environ[\'DJANGO_SECRET_KEY\'] = \'{new_secret}\'" >> env_var.py')
 
 
 def _create_or_update_dotenv(c, site_name):
@@ -105,7 +125,7 @@ def _update_database(c):
     c.run('~/venvs/tdd-django/bin/python manage.py migrate --noinput')
 
 
-def _move_devil_passenger_config(c):
+def _copy_devil_passenger_config(c):
     c.run('cp deploy_tools/passenger_wsgi.py .')
 
 
