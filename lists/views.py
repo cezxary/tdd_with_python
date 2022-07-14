f2ffadee7d3f31936d4c5d0b1df264d255f333d1
cezxary <74569453+cezxary@users.noreply.github.com>
2022-07-14 20:27:38 +0200

    Enfoce model validation in list view

--- lists/views.py
+++ lists/views.py
@@ -7,12 +7,21 @@ def home_page(request):
     return render(request, 'lists/home.html')
 
 
+# TODO: remove duplication of validation logic in views
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    error = None
+
     if request.method == 'POST':
-        Item.objects.create(list=list_, text=request.POST['item_text'])
-        return redirect(f'/lists/{list_.id}/')
-    return render(request, 'lists/list.html', {'list': list_})
+        try:
+            item = Item(list=list_, text=request.POST['item_text'])
+            item.full_clean()
+            item.save()
+            return redirect(f'/lists/{list_.id}/')
+        except ValidationError:
+            error = "You can't have an empty list item"
+
+    return render(request, 'lists/list.html', {'list': list_, 'error': error})
 
 
 def new_list(request):
