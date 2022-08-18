5889d6a0a36a4e1f8e3629734134a55b158f775b
cezxary <74569453+cezxary@users.noreply.github.com>
2022-08-18 22:34:10 +0200

    Duplicate items check for existing lists form used in list view. FT passes

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -57,5 +57,5 @@ class ItemValidationTest(FunctionalTest):
         # She sees a helpful error message
         self.wait_for(lambda: self.assertEqual(
             self.browser.find_element(By.CSS_SELECTOR, '.has-error').text,
-            "You've already got this in your list"
+            "You've already got this on your list"
         ))
